54aee1fb8bde1bc8cf88e2d0e380739502d11807
tiganko <Mohammed.Eltigani.97@gmail.com>
2022-12-30 22:07:03 +0200

    just tried adding a couple lines

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,5 +8,7 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+        System.out.println("This is my very first git hub app");
+        System.out.println("I created it just to know how GitHub works");
     }
 }
